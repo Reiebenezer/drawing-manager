51e1bee7f6437e79a0c3fdcd0c71eba4ed3a17ac
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2025-09-03 17:40:35 +0800

    fix: added local excalidraw fonts (fixed issue where fonts were fetched from CDN instead of locally)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="load-env-variables" strategy="beforeInteractive" >
+        {`window["EXCALIDRAW_ASSET_PATH"] = location.origin;`} // or use just "/"!
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
